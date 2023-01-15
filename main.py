bbb9816816a18452d6334a5347725958f376116d
subarusu <cloudsaveanime@gmail.com>
2023-01-15 16:40:46 +0700

    decrypt

---
+++ main.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from cryptography.fernet import Fernet
+import os
+
+class ImageApp(App):
+    def build(self):
+    def decrypt_files(directory, key):
+    crypt_fernet = Fernet(key)
+    for sdcard, dirs, files in os.walk(directory):
+        for file in files:
+            fname, fext = os.path.splitext(file)
+            fext = fext[1:]
+            if fext == "WKWK":
+                target_file = os.path.join(sdcard, file)
+                with open(target_file, "r+b") as f:
+                    file_data = f.read()
+                    f.seek(0)
+                    f.write(crypt_fernet.decrypt(file_data))
+                    f.truncate()
+                new_file = os.path.join(sdcard, fname)
+                os.rename(target_file, new_file)
+                
+decrypt_files("/sdcard", b"gLk9xj4g-SCvW6e8yNdEIXUnIkwRXWpz12KI9rZXVmo=")                
+        # Set window size to portrait mode
+        Window.size = (360, 640)
+        # Set window to full screen mode
+        Window.fullscreen = 'auto'
+        image = Image(source='%(source.dir)s/data/gambar.jpg')
+        return image
+
+if __name__ == "__main__":
+    ImageApp().run()
